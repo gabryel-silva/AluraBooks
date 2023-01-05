603fdc658c19e5b06f4fc60351d6064a86013361
Gabryel Silva <gabryelssantos@gmail.com>
2023-01-05 19:40:03 -0300

    Versão 1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alura Books
+* Neste projeto foi desenvolvido filtros para livros de front-end, back-end, livros de dados, livros disponíveis, e tudo isso com a manipulação de elementos na tela através do DOM e o JavaScript.
+[link do projeto]()
---
+++ app/metodoFilter.js
@@ -0,0 +1,29 @@
+const botoes = document.querySelectorAll('.btn');
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros(){
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value;
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
+    exibirLivrosNaTela(livrosFiltrados);
+    if(categoria == 'disponivel'){
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+        exibirTotalDoslivrosDisponiveisNaTela(valorTotal);
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirTotalDoslivrosDisponiveisNaTela(valorTotal){
+    elementoComValorTotalDosLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+}
